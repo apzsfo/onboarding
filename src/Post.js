3faf44b3cd34598ae150566a174728265e15ae7e
Andrew Zeff <andrewzeff@Andrews-MacBook-Pro.local>
2020-11-14 02:22:26 -0800

    integrated with firestore

--- src/Post.js
+++ src/Post.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Post() {
-  const [text, setText] = useState(''); // use empty string as our initial value
-  const [displayText, setDisplayText] = useState(''); // text that is actually displayed to the screen
-
+export default function Post({ title, body, author }) {
+  //   const [text, setText] = useState(''); // use empty string as our initial value
+  // text that is actually displayed to the screen
+  //   const [displayText, setDisplayText] = useState('');
   // return an input that displays our text whenever we click the button
+  const post = `${title}: ${body} - ${author}`;
+  //   console.log(displayText);
   return (
     <>
       <div>
-        {displayText}
+        {post}
       </div>
-      <input onChange={(e) => { setText(e.target.value); }} />
-      <button type="button" onClick={() => { setDisplayText(text); }}>Click me!</button>
+      {/* <input onChange={(e) => { setText(e.target.value); }} /> */}
+      {/* <button type="button" onClick={() => { setDisplayText(text); }}>Click me!</button> */}
     </>
   );
 }
+
+Post.propTypes = {
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
